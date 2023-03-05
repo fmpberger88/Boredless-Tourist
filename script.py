dfaf66592a6458cc082846e94e4111979fb1cdb0
fmpberger <fabian.berger@protonmail.ch>
2023-03-05 22:05:53 +0100

    Created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
-destinations = ['Paris, France', 'Shangai, China', 'Los Angeles, USA', 'Sao Paulo, Brazil', 'Cairo, Egypt']
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paulo, Brazil', 'Cairo, Egypt']
 
-test_traveler = ['Erin Wilkes', 'Shangai, China', ['historical site', 'art']]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 
 def get_destination_index(destination):
@@ -15,3 +15,27 @@ def get_traveler_location(traveler):
 
 test_destination_index = get_traveler_location(test_traveler)
 print(test_destination_index)
+
+attractions = [[] for i in destinations]
+print(attractions)
+
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions[destination_index].append(attraction)
+    except:
+        print("Location does not exist!")
+
+add_attraction("Los Angeles, USA", ['Venice Beach',['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("Sao Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("Sao Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(attractions)
